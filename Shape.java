5d96ae9498f117226df4268913516217e5f7e3c8
vaishnavisirul <32641192+vaishnavisirul@users.noreply.github.com>
2019-05-01 15:16:45 +0200

    all classes with get and set methods

--- Shape.java
+++ Shape.java
@@ -1,11 +1,4 @@
-package svg.element.Shape;
-import svg.element.*;
 
-public abstract class Shape extends BaseElement
-{
-    public Shape(String shapeType)
-    {
-        super(shapeType);
-    }
-
+public abstract class Shape extends BaseElement {
+	
 }
