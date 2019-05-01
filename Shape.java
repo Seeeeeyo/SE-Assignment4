9b58b7696b4f7c83435f0075b10eeaa9f2707cd3
Benjamin Rodrigues de Miranda <rodriguesdemiranda99@gmail.com>
2019-05-01 14:30:40 +0200

    Abstract class shape

---
+++ Shape.java
@@ -0,0 +1,11 @@
+package svg.element.Shape;
+import svg.element.*;
+
+public abstract class Shape extends BaseElement
+{
+    public Shape(String shapeType)
+    {
+        super(shapeType);
+    }
+
+}
