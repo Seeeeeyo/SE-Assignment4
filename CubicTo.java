e75ed055da8971f762694fd1e712371a7d190ce5
Janneke13 <45425403+Janneke13@users.noreply.github.com>
2019-05-01 22:34:58 +0200

    added some changes to the load method

--- CubicTo.java
+++ CubicTo.java
@@ -1,64 +1,65 @@
+package svg.shape;
 
-public class CubicTo {
+import svg.SVGParser;
+import svg.element.Element;
+
+public class CubicTo extends PathOp{
 	private double x1;
-	private double y1;
 	private double x2;
-	private double y2;
 	private double x3;
+	private double y1;
+	private double y2;
 	private double y3;
 	
+	 
 	public CubicTo()
 	{
-
-	}
-	public double getX1()
-	{
-		return x1;
-	}
-	public double getX2()
-	{
-		return x2;
-	}
-	public double getX3()
-	{
-		return x3;
-	}
-	public double gety1()
-	{
-		return y1;
-	}
-	public double gety2()
-	{
-		return y2;
-	}
-	public double gety3()
-	{
-		return y3;
-	}
-	public void setX1(double x1)
-	{
-		this.x1 = x1;
-	}
-	public void setX2(double x2)
-	{
-		this.x2 = x2;
-	}
-	public void setX3(double x3)
-	{
-		this.x3 = x3;
-	}
-	public void sety1(double y1)
-	{
-		this.y1 = y1;
-	}
-	public void sety2(double y2)
-	{
-		this.y2 = y2;
-	}
-	public void sety3(double y3)
-	{
-		this.y3 = y3;
-	}
+		super();
 		
+	}
+	public Element newInstance() {
+		return new CubicTo();
+	}
+	
+	public boolean load(final String expr) {
+		if (expr.contains("x1=")) {
+			final Double result = SVGParser.extractDouble(expr, "x1=");
+			if (result != null) {
+				x1 = result.doubleValue();
+			}
+		}
+		if (expr.contains("y1=")) {
+			final Double resultTwo = SVGParser.extractDouble(expr, "y1=");
+			if (resultTwo != null) {
+				y1 = resultTwo.doubleValue();
+			}
+		}
+		if (expr.contains("x2=")) {
+			final Double resultThree = SVGParser.extractDouble(expr, "x2=");
+			if (resultThree != null) {
+				x2 = resultThree.doubleValue();
+			}
+		}
+		if (expr.contains("y2=")) {
+			final Double resultFour = SVGParser.extractDouble(expr, "y2=");
+			if (resultFour != null) {
+				y2 = resultFour.doubleValue();
+			}
+		}
+		if (expr.contains("x3=")) {
+			final Double resultFive = SVGParser.extractDouble(expr, "x3=");
+			if (resultFive != null) {
+				x3 = resultFive.doubleValue();
+			}
+		}
+		if (expr.contains("y3=")) {
+			final Double resultSix = SVGParser.extractDouble(expr, "y3=");
+			if (resultSix != null) {
+				y3 = resultSix.doubleValue();
+			}
+		}
+		return false;
+	} 
+
 	
 }
